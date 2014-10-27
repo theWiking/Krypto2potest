f74e3ea89d6a51b7f8b6a2eeae8df9cd100781b5
theWiking <miodekip@gmail.com>
2014-10-27 23:23:26 +0100

    tekst
    kodowanie klucza skończone teraz zabieram się za tekst jestem przy 4 (d)
    - nie wiem jak to zrobic

--- krypto2i.cpp
+++ krypto2i.cpp
@@ -5,7 +5,21 @@
 
 using namespace std;
 int main(){
-  bool w[8]={1,1,1,1,0,0,0,0};
+  bool sbox1[16][2]={{0,1},{1,1},{0,0},{1,0},{1,1},{0,1},{1,0},{0,0},{0,0},{1,1},{0,1},{0,1},{0,1},{1,1},{1,1},{1,0}};
+  bool sbox2[16][2]={{0,0},{1,0},{0,1},{0,0},{1,0},{0,1},{1,1},{1,1},{1,1},{1,0},{0,0},{0,1},{0,1},{0,0},{0,0},{1,1}};
+
+
+  //zmienne do tekstu
+  bool t[8]={1,0,1,1,1,0,0,0};
+  bool t1[4]={0};
+  bool t2[4]={0};
+  bool t2c1[4]={0};
+  bool t2c2[4]={0};
+  bool pop4w8[8]={0};
+  int p4w8[8]={3,0,1,2,1,2,3,0};
+  bool poDodawaniu[8]={0};
+
+  //zmienne do klucza
   bool kp[10]={1,1,0,0,0,0,0,0,1,1};
   int P10[10]={2,4,1,6,3,9,0,8,7,5};
   bool poP10[10]={0};
@@ -19,22 +33,56 @@ int main(){
   bool k22[5]={0};
   int Przez2[5]={2,3,4,0,1};
   bool k12k22[10]={0};
+  bool poP10w82[8]={0};
 
+
+  //klucz
+  cout<<"Permutacja P10: \t";
   Per10(kp,P10,poP10);
     for(int i=0;10>i;i++){
       cout<<poP10[i];
     }
   cout<<endl;
+  cout<<"czesci polaczone:\t";
   PrzezIPodzia(poP10,k1,k2,Przez1);
   LaczeniePolowek(k1,k2,k1k2);
   for(int i=0;10>i;i++){
       cout<<k1k2[i];
   }
   cout<<endl;
+  cout<<"Klucz 1 rundy:   \t";
   Per10w8(k1k2,poP10w8,p10w8);
   for(int i=0;8>i;i++){
     cout<<poP10w8[i];
   }
   cout<<endl;
+  cout<<"2runda po podzieleniu:\t";
+  PrzezIPodzia(k1k2,k12,k22,Przez2);
+  LaczeniePolowek(k12,k22,k12k22);
+  for(int i=0;10>i;i++){
+      cout<<k12k22[i];
+  }
+  cout<<endl;
+  cout<<"Klucz 2 rundy:   \t";
+  Per10w8(k12k22,poP10w82,p10w8);
+  for(int i=0;8>i;i++){
+    cout<<poP10w82[i];
+  }
+  cout<<endl<<endl<<endl<<"Tekst"<<endl;
+
+  //tekst
+  cout<<"Kopia 1 P4w8:   \t";
+  podzielTekst(t,t1,t2);
+  zrob2Kopie(t2,t2c1,t2c2);
+  P4w8(t2c1,pop4w8,p4w8);
+  for(int i=0;8>i;i++)
+    cout<<pop4w8[i];
+  cout<<endl;
+  cout<<"Dodawanie Symetryczne:\t";
+  dodawanieSymetryczne(poP10w8,pop4w8,poDodawaniu);
+  for(int i=0;8>i;i++)
+    cout<<poDodawaniu[i];
+  cout<<endl;
+
 return 0;
 }
--- tekst.h
+++ tekst.h
@@ -2,3 +2,25 @@
 #include "zmienne.h"
 
 using namespace std;
+void podzielTekst(bool t[8],bool t1[4],bool t2[4]){
+  for(int i=0;8>i;i++){
+    t1[i]=t[i];
+    t2[i]=t[i+4];
+    }
+}
+void zrob2Kopie(bool t1[4],bool t1c1[4],bool t1c2[4]){
+  for(int i=0;4>i;i++)
+    t1c1[i]=t1c2[i]=t1[i];
+}
+void P4w8(bool t1c1[4],bool pop4w8[8],int p4w8[8]){
+  for(int i=0;8>i;i++)
+    pop4w8[i]=t1c1[p4w8[i]];
+}
+void dodawanieSymetryczne(bool tekst[8],bool klucz[8],bool wyjscie[8]){
+    for(int i=0;i<8;i++)
+        {if(tekst[i]==1 && klucz[i]==1)
+            wyjscie[i]=0;
+        else
+        wyjscie[i]=tekst[i]+klucz[i];
+}
+}
